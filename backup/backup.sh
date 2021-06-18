33ed3d5c63badd1c7688de41f432d54e2a242b7f
BrandonWade <brandon_wade2005@hotmail.com>
2021-06-18 16:38:34 -0500

    Update backup script to output file.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
 echo "db backup"
+TSTAMP=$(date +"%Y%d%m")
+
+mysqldump -uroot -proot -hblackblade-db blackblade | gzip > "$TSTAMP.sql.gz"
+
+# To unzip a backup
+# gzip -dk "$TSTAMP.sql.gz"
