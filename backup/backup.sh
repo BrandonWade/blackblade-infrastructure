7cfcbef3e478e2ecfbc107d4b319615fb61d5a46
BrandonWade <brandon_wade2005@hotmail.com>
2021-06-20 00:03:03 -0500

    Update backup script to upload db dump to Digital Ocean.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "db backup"
+echo "backing up database..."
+START=$(date +%s)
 TODAY=$(date +"%Y%m%d")
 
 # Dump the db and compress the resulting file
-mysqldump -uroot -proot -hblackblade-db blackblade | gzip > "$TODAY.sql.gz"
+mysqldump -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOST} ${DB_DATABASE} | gzip > "${TODAY}.sql.gz"
 
-# To unzip a backup
-# gzip -dk "$TODAY.sql.gz"
+# Upload the file to the Digital Ocean space
+aws s3 mv "${TODAY}.sql.gz" s3://${DO_SPACE_NAME}/ --endpoint=https://${DO_SPACE_ENDPOINT}
+
+END=$(date +%s)
+RUNTIME=$(date -d@$((END - START)) -u +%Hh%Mm%Ss)
+echo "backup successfully completed in ${RUNTIME}"
