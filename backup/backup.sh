3c5f5be73abb974e96ddf1a0f15ce51ecd173ad7
BrandonWade <brandon_wade2005@hotmail.com>
2021-06-20 01:46:34 -0500

    Upload backups to environment folders.

--- backup/backup.sh
+++ backup/backup.sh
@@ -8,7 +8,7 @@ TODAY=$(date +"%Y%m%d")
 mysqldump -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOST} ${DB_DATABASE} | gzip > "${TODAY}.sql.gz"
 
 # Upload the file to the Digital Ocean space
-aws s3 mv "${TODAY}.sql.gz" s3://${DO_SPACE_NAME}/ --endpoint=https://${DO_SPACE_ENDPOINT}
+aws s3 mv "${TODAY}.sql.gz" s3://${DO_SPACE_NAME}/${ENVIRONMENT}/ --endpoint=https://${DO_SPACE_ENDPOINT}
 
 END=$(date +%s)
 RUNTIME=$(date -d@$((END - START)) -u +%Hh%Mm%Ss)
